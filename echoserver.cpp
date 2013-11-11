f899c2b909f056f3ff19e707da67ef9d12845cb5
venuatu <venuatu@gmail.com>
2013-11-11 14:58:06 +1100

    echoserver: switch to forking server and extract try catch for constructor into function

--- echoserver.cpp
+++ echoserver.cpp
@@ -1,8 +1,24 @@
+// A forking echo server
 #include <cstdlib>
 #include <iostream>
 #include <string>
 using namespace std;
 #include "socket.h"
+#include "unistd.h"
+
+ServerSocket create_serverSocket(short port)
+{
+    try 
+    {
+        ServerSocket sock(port);
+        return sock;
+    }
+    catch (exception& e)
+    {
+        cerr << "Socket error: " << e.what() << endl;
+        exit(1);
+    }
+}
 
 int main (int argc, char** argv)
 {
@@ -10,27 +26,25 @@ int main (int argc, char** argv)
     if (argc > 1)
         port = atoi(argv[1]);
 
-    try
+    cout << "listening on " << port << endl;
+    signal(SIGCHLD, SIG_IGN);
+    ServerSocket server = create_serverSocket(port);
+
+    while(true)
     {
-        ServerSocket server(port);
-        cout << "listening on " << port << endl;
+        Socket sock = server.accept();
+        cout << "New client from: " << sock.remoteHost() << endl;
 
-        while(true)
+        if (fork() == 0)
         {
-            Socket sock = server.accept();
-            cout << "New client from: " << sock.remoteHost() << endl;
-
             string buffer;
             while (!(buffer = sock.read()).empty())
             {
-                cout << "Echoing: " << buffer << endl;
+                cout << "    " << sock.remoteHost() << ": " << buffer << endl;
                 sock.write(buffer);
             }
             cout << "Connection closed: " << sock.remoteHost() << endl;
+            exit(0);            
         }
-    }
-    catch (exception& e)
-    {
-        cout << "Error: " << e.what() << endl;
     }
 }
